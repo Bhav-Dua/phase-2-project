eda418d8e9f1b84289b2bea712347b67241c37b3
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-21 16:27:52 -0500

    Add logo

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../apexLogo.png";
+
+function Header() {
+    return (
+        <img src={logo} alt="Apex Legends" />
+    )
+}
+
+export default Header;
