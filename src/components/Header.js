7038bdced9ac9310589b4c29efbe0c6da530d875
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-21 18:19:42 -0500

    Implement NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "../apexLogo.png";
+import NavBar from "./NavBar";
 
 function Header() {
     return (
+      <div className="header">
         <img src={logo} alt="Apex Legends" />
+        <NavBar />
+      </div>
+        
     )
 }
 
