c60fac2454e8b2419d1bf3c7451724975f2369b1
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-21 18:25:37 -0500

    Update styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import NavBar from "./NavBar";
 function Header() {
     return (
       <div className="header">
-        <img src={logo} alt="Apex Legends" />
+        <img className="logo" src={logo} alt="Apex Legends" />
         <NavBar />
       </div>
         
