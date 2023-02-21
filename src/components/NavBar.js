352ea36f0afc1c2b16dbf9290a06adf2a9a3d2d0
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-21 18:41:28 -0500

    Update links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,7 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
     return (
         <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/legends">Legends</NavLink>
+            <NavLink to="/">Legends</NavLink>
             <NavLink to="/newLegend">Add A Legend</NavLink>
             <NavLink to="/mostLiked">Most Liked</NavLink>
         </nav>
