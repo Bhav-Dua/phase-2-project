b2d909454e13b7e55a351a85761dee0242a4d0df
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-21 19:12:47 -0500

    Add fetch for legends data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
 import '../App.css';
 import Header from './Header';
 
 function App() {
+
+  const [legends, setLegends] = useState([]);
+
+  fetch("http://localhost:3001/legends")
+    .then(r => r.json())
+    .then(setLegends)
+
+
   return (
     <div className="App">
       <Header />
