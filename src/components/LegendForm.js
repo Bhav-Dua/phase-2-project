9e97be663ca15f675e5ada3c3f4b5e52719c8dc8
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-23 17:35:19 -0500

    Add fetch request

--- src/components/LegendForm.js
+++ src/components/LegendForm.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-function LegendForm() {
+function LegendForm({ addLegend }) {
+
+    const [formData, setFormData] = useState({name: "", tactical: "", ultimate: "", image: ""})
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        fetch("http://localhost:3001/legends", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(r => r.json())
+            .then(addLegend)
+
+    }
+
+    function handleFormData(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label for="name">Name: </label>
-            <input type="text" id="name" name
-        
+            <input type="text" id="name" name="name" value={formData.name} onChange={handleFormData} />
+            <br />
+            <label for="tactical" />
+            <input type="text" id="tactical" name="tactical" value={formData.tactical} onChange={handleFormData} />
+            <br />
+            <label for="ultimate" />
+            <input type="text" id="ultimate" name="ultimate" value={formData.ultimate} onChange={handleFormData} />
+            <br />
+            <label for="image" />
+            <input type="text" id="image" name="image" value={formData.image} onChange={handleFormData} />
+            <br />
+            <input type="submit" value="Submit" />
         </form>
     )
 }
