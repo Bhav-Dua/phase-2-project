aec3305c4c3763a000fbc7697ad756beaf2ee1f6
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-23 17:29:37 -0500

    Create LegendForm

---
+++ src/components/LegendForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LegendForm() {
+    return (
+        <form>
+            <label for="name">Name: </label>
+            <input type="text" id="name" name
+        
+        </form>
+    )
+}
+
+export default LegendForm;
