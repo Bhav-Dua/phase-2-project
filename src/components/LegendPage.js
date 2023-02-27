5faa6a75668ccad3bfc78388c7166137738edcc4
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-27 15:32:21 -0500

    Add legend details

--- src/components/LegendPage.js
+++ src/components/LegendPage.js
@@ -1,18 +1,33 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function LegendPage() {
 
     const [legend, setLegend] = useState(null);
-
-    const id = 1;
+    const { id } = useParams();
 
     useEffect(() => {
         fetch(`http://localhost:3001/legends/${id}`)
             .then(r => r.json())
-            .then(data => console.log(data))
+            .then(setLegend)
     }, [id])
 
-
+    return (
+        <div className="legend_Page">
+            <div className="legend_Image">
+                <img src={legend.image} alt={legend.name} />
+            </div>
+            <div className="legend_Content">
+                <h1>{legend.name}</h1>
+                <h2>Abilities</h2>
+                <ul>
+                    <li><b>Passive: </b>{legend.passive}</li>
+                    <li><b>Tactical: </b>{legend.tactical}</li>
+                    <li><b>Ultimate: </b>{legend.ultimate}</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default LegendPage;
