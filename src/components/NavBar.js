69f5cdd17e9f335fb5d502310ac21322b6e7c795
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-27 17:23:13 -0500

    Add class

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
     return (
         <nav className="navbar">
-            <NavLink to="/">Legends</NavLink>
-            <NavLink to="/newLegend">Add A Legend</NavLink>
+            <NavLink className="link" to="/">Legends</NavLink>
+            <NavLink className="link" to="/newLegend">Add A Legend</NavLink>
         </nav>
     )
 }
