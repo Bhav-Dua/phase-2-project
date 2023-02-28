409bbb2ccc4970e9330d2ec2532a34479d59afd1
Bhavya Dua <dua_bhavya@outlook.com>
2023-02-27 22:02:00 -0500

    Update css

--- src/components/LegendForm.js
+++ src/components/LegendForm.js
@@ -32,23 +32,28 @@ function LegendForm({ addLegend }) {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form className="ui form" onSubmit={handleSubmit}>
+            <div className="field">
             <label htmlFor="name">Name </label>
             <input type="text" id="name" name="name" value={formData.name} onChange={handleFormData} />
-            <br />
+            </div>
+            <div className="field">
             <label htmlFor="passive">Passive </label>
             <input type="text" id="passive" name="passive" value={formData.passive} onChange={handleFormData} />
-            <br />
+            </div>
+            <div className="field">
             <label htmlFor="tactical">Tactical </label>
             <input type="text" id="tactical" name="tactical" value={formData.tactical} onChange={handleFormData} />
-            <br />
+            </div>
+            <div className="field">
             <label htmlFor="ultimate">Ultimate </label>
             <input type="text" id="ultimate" name="ultimate" value={formData.ultimate} onChange={handleFormData} />
-            <br />
+            </div>
+            <div className="field">
             <label htmlFor="image">Image URL </label>
             <input type="text" id="image" name="image" value={formData.image} onChange={handleFormData} />
-            <br />
-            <input type="submit" value="Submit" />
+            </div>
+            <button className="ui button" type="submit">Submit</button>
         </form>
     )
 }
